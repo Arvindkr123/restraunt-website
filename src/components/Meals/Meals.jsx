ff848b1895496e14ad6fce459e258b91c74a70c7
Arvind <thakurarvindkr10@gmail.com>
2023-05-09 14:23:43 +0530

    Add Restaurant Summary

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSummary from './MealsSummary'
+import AvailableMeal from './AvailableMeal'
+
+const Meals = () => {
+    return (
+        <>
+            <MealsSummary />
+            <AvailableMeal/>
+        </>
+    )
+}
+
+export default Meals
