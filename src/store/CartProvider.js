c45a4baa3fb1477b06c7e5c0fd1b4730fe645b66
Arvind <thakurarvindkr10@gmail.com>
2023-05-12 11:17:29 +0530

    added  remove items functionality also from the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -24,11 +24,34 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = state.items.concat(action.item);
     }
-
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
+  }
+
+  if (action.type === 'REMOVE') {
+    // first find the index of object which you want to delete....
+    const existingItemIndex = state.items.findIndex(item => item.id === action.id);
+    // now get that item ...
+    const existingItem = state.items[existingItemIndex];
+    // and then decrease the amount
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      // if amount if is bigger than one then do this
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount : updatedTotalAmount
+    }
+
   }
   return defaultCartState;
 };
